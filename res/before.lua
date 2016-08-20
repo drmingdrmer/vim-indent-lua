feca380f447ebc71994b6923b735593b651e8f97
drdr xp <drdr.xp@gmail.com>
2016-08-20 00:38:34 -0700

    add comparison

---
+++ res/before.lua
@@ -0,0 +1,38 @@
+
+    function foo(a, b,
+        c)
+    end
+
+    function foo(
+        a, b)
+    end
+
+    a = {a,
+    b}
+
+    {
+        a = foo(
+        a, b, {
+            1, 2, 3
+        })
+    }
+
+    if (a == b
+        and (c == d
+        or c == e))
+        then
+            doit()
+        end
+
+    a, b, c =
+    function_name(x,
+    y, z)
+
+    x = y +
+    c +
+    variable
+
+    return { expr },
+    err, err_mes
+
+-- vim: foldlevel=10
